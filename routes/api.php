a9847c79ebd49416008a5eac6c91dd8ce973b2a2
unknown <diana2809@gmail.com>
2026-09-07 10:07:32 +0700

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\ShippingController;
+
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/payment/create', [PaymentController::class, 'create']);
+Route::get('/shipping/destinations', [ShippingController::class, 'destinations']);
+Route::post('/shipping/cost', [ShippingController::class, 'cost']);
