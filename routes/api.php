1e37420dadb42ee72971ea3568c5f82d3284e7f6
unknown <diana2809@gmail.com>
2026-09-07 08:17:01 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\PaymentController;
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/payment/create', [PaymentController::class, 'create']);
